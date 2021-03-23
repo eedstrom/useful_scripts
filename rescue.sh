d83580e24141d5163d6d3869c65857b0e1acefb8
Eric S. Edstrom <ericedstrom123@gmail.com>
2021-03-22 21:19:15 -0500

    Add comment to rescue.sh

--- rescue.sh
+++ rescue.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# Script for mounting rescue CD
+# Script for mounting rescue CD.  Pretty nifty
 
 mount -o subvol=@,ssd,noatime,compress=zstd:3,space_cache,commit=120 /dev/nvme0n1p3 /mnt
